f7b7ea484565c3c9beb54e9b735f9fba979a2ac6
z003tnxs <z003tnxs@md1ta5bc.ad001.siemens.net>
2019-12-10 01:06:46 +0530

    Taking java files & testng xml files only

---
+++ Cutomized_Excel_Report/src/excel_Report/First_Test_Class.java
@@ -0,0 +1,26 @@
+package excel_Report;
+
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+ 
+public class First_Test_Class
+{
+    @Test
+    public void testMethodPass()
+    {
+     Assert.assertTrue(true);
+    }
+     
+    @Test
+    public void testMethodFail()
+    {
+     Assert.assertTrue(false);
+    }
+     
+    @Test
+    public void testMethodSkip()
+    {
+     throw new SkipException("Skipped Intentionally");
+    }
+}
---
+++ Cutomized_Excel_Report/src/generate/Excel.java
@@ -0,0 +1,13 @@
+package generate;
+
+import org.automationtesting.excelreport.Xl;
+
+public class Excel {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+       
+		Xl.generateReport("excel-report.xlsx");
+	}
+
+}
